40231cccd33c53dc8580eb873d1b6333a81d9b80
paulobetbeder <190793038+paulobetbeder@users.noreply.github.com>
2025-01-09 13:00:46 -0300

    Rotina para modelo BSM univariado

---
+++ 7_Modelo BSM Univariado 1 região.R
@@ -0,0 +1,14 @@
+################################################################################
+##                          MODELO BSM UNIVARIADO                             ##
+################################################################################
+
+library(dlm)
+library(tidyverse)
+library(beepr)
+
+### TESTE PARA BELO HORIZONTE ##################################################
+
+## Carregando Base e definindo objeto para BH
+
+base <- readRDS("D:/FJP2425/Programacao/data/baserot0324.RDS")
+
