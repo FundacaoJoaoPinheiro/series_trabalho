eb63b7e96a69c4d34fa5052e8777100210bdea37
paulobetbeder <190793038+paulobetbeder@users.noreply.github.com>
2025-05-27 22:28:34 -0300

    Create 28_Comparativo multivariado com univariado - Desoc.R

---
+++ 28_Comparativo multivariado com univariado - Desoc.R
@@ -0,0 +1,357 @@
+################################################################################
+## COMPARANDO RESULTADOS DO MODELO MULTIVARIADO COM MODELO UNIVARIADO - DESOC ##
+################################################################################
+
+# A PRINCÍPIO, NESTE SCRIPT OS RESUTADOS FORAM MODIFICADOS APENAS PARA BH e VALE
+
+## Para visualizar os gráficos em segunda tela:
+
+dev.new()
+dev.new()
+
+
+### 01 - BELO HORIZONTE ########################################################
+rm(list = ls())
+gc()
+
+env1<-new.env()
+env2<-new.env()
+
+load("D:/FJP2425/Programacao/data/Rdatas/6_estruturaldesocup_8reg/01_mod_bh.Rdata", envir = env1)
+load("D:/FJP2425/Programacao/data/Rdatas/11_multivariado_semcorr - desoc_8reg/02_mod_semcorr_SULARMA11.Rdata", envir = env2)
+
+baseestr8reg <- readRDS("D:/FJP2425/Programacao/data/baseestr8reg.RDS")
+bh<-baseestr8reg$`01-Belo Horizonte`
+baseal8reg<- readRDS("D:/FJP2425/Programacao/data/basealinhada_8reg.RDS")
+dtbh<-baseal8reg$`01-Belo Horizonte` 
+dbbh<-readRDS("D:/FJP2425/Programacao/data/pseudoerros_8reg/01_params_bh.RDS")
+
+desoc_bh <- bh$Total.de.desocupados/1000
+se_db<- bh$sd_d/1000
+cv_bh <- se_db/desoc_bh
+ICinf_bh<-desoc_bh-1.96*se_db
+ICsup_bh<-desoc_bh+1.96*se_db
+
+desoc_bh <- window(ts.union(ts(desoc_bh, start = 2012, frequency = 4)), start = c(2013,4))
+ICinf_bh <- window(ts.union(ts(ICinf_bh, start = 2012, frequency = 4)), start = c(2013,4))
+ICsup_bh <- window(ts.union(ts(ICsup_bh, start = 2012, frequency = 4)), start = c(2013,4))
+cv_bh <- window(ts.union(ts(cv_bh, start = 2012, frequency = 4)), start = c(2013,4))
+
+estsinal_ma1bh <- env1$ma1_bh$ts.signal
+estsinal_ma1bh <- window(ts.union(ts(estsinal_ma1bh, start = 2012, frequency = 4)), start = c(2013,4))
+estcv_ma1bh <- env1$ma1_bh$cv.signal
+estcv_ma1bh <- window(ts.union(ts(estcv_ma1bh, start = 2012, frequency = 4)), start = c(2013,4))
+
+multsinal_semcorr_bh <- env2$modelo_mult_sem_corr$ts.signal_1
+multsinal_semcorr_bh <- window(ts.union(ts(multsinal_semcorr_bh, start = 2012, frequency = 4)), start = c(2013,4))
+cv_multsinal_semcorr_bh <- env2$modelo_mult_sem_corr$cv.signal_1
+cv_multsinal_semcorr_bh <- window(ts.union(ts(cv_multsinal_semcorr_bh, start = 2012, frequency = 4)), start = c(2013,4))
+
+par(mfrow=c(1,2), mar=c(5,5,1,1), oma=c(0,0,2,0), cex=0.8)
+plot(desoc_bh, type = "l", col = "black", lwd = 2,
+     xlab = "Ano", ylab = "Total de desocupados (milhares de pessoas)", ylim=c(40,245))
+lines(estsinal_ma1bh, col = "blue", lty = 1, lwd = 2)
+lines(multsinal_semcorr_bh, col= "red", lty = 1, lwd = 2)
+lines(ICinf_bh, col = "black", lty = 2)
+lines(ICsup_bh, col = "black", lty = 2)
+legend("topleft", legend = c("Desocupação", "Sinal univariado - MA(1)","Sinal multivariado - MA(1)",
+                             "IC 95% - estimativa direta"), 
+       col = c("black","blue","red","black"),lty = c(1,1,1,2),lwd = c(2,2,2,1),bty = "n", cex=0.8)
+
+plot((cv_bh*100), type = "l", col = "black", lwd = 2,
+     xlab = "Ano", ylab = "CV (%)",  ylim=c(3,15))
+lines(estcv_ma1bh, col = "blue",lwd=2, lty = 1)
+lines(cv_multsinal_semcorr_bh, col = "red",lwd=2,lty = 1)
+legend("topleft", legend = c("CV desocupados","CV sinal - univariado", "CV sinal - multivariado"), 
+       col = c("black","blue", "red"),lty = c(1,1,1),lwd = c(2,2,2),bty = "n", cex=0.8)
+mtext("01 - Belo Horizonte", side = 3, outer = TRUE, line = 0.5, font = 2, cex = 1.2)
+
+
+### 02 - COLAR E ENTORNO METROPOLITANO DE BELO HORIZONTE #######################
+rm(list = ls())
+
+env1<-new.env()
+env2<-new.env()
+
+load("D:/FJP2425/Programacao/data/Rdatas/6_estruturaldesocup_8reg/02_mod_ent.Rdata", envir = env1)
+load("D:/FJP2425/Programacao/data/Rdatas/11_multivariado_semcorr - desoc_8reg/02_mod_semcorr_SULARMA11.Rdata", envir = env2)
+
+baseestr8reg <- readRDS("D:/FJP2425/Programacao/data/baseestr8reg.RDS")
+ent<-baseestr8reg$`02-Colar e Entorno metropolitano de BH`
+baseal8reg<- readRDS("D:/FJP2425/Programacao/data/basealinhada_8reg.RDS")
+dtent<-baseal8reg$`02-Colar e Entorno Metropolitano de BH`
+dbent<-readRDS("D:/FJP2425/Programacao/data/pseudoerros_8reg/02_params_ent.RDS")
+
+desoc_ent <- (ent$Total.de.desocupados)/1000
+se_db <- (ent$sd_d)/1000
+cv_ent <- se_db/desoc_ent
+ICinf_ent<-desoc_ent-1.96*se_db
+ICsup_ent<-desoc_ent+1.96*se_db
+
+desoc_ent <- window(ts.union(ts(desoc_ent, start = 2012, frequency = 4)), start = c(2013,4))
+ICinf_ent <- window(ts.union(ts(ICinf_ent, start = 2012, frequency = 4)), start = c(2013,4))
+ICsup_ent <- window(ts.union(ts(ICsup_ent, start = 2012, frequency = 4)), start = c(2013,4))
+cv_ent <- window(ts.union(ts(cv_ent, start = 2012, frequency = 4)), start = c(2013,4))
+
+estsinal_ma1ent <- env1$ma1_ent$ts.signal
+estsinal_ma1ent <- window(ts.union(ts(estsinal_ma1ent, start = 2012, frequency = 4)), start = c(2013,4))
+estcv_ma1ent <- env1$ma1_ent$cv.signal
+estcv_ma1ent <- window(ts.union(ts(estcv_ma1ent, start = 2012, frequency = 4)), start = c(2013,4))
+
+multsinal_semcorr_ent <- env2$modelo_mult_sem_corr$ts.signal_2
+multsinal_semcorr_ent <- window(ts.union(ts(multsinal_semcorr_ent, start = 2012, frequency = 4)), start = c(2013,4))
+cv_multsinal_semcorr_ent <- env2$modelo_mult_sem_corr$cv.signal_2
+cv_multsinal_semcorr_ent <- window(ts.union(ts(cv_multsinal_semcorr_ent, start = 2012, frequency = 4)), start = c(2013,4))
+
+par(mfrow=c(1,2), mar=c(5,5,1,1), oma=c(0,0,2,0), cex=0.8)
+plot(desoc_ent, type = "l", col = "black", lwd = 2,
+     xlab = "Ano", ylab = "Total de desocupados (milhares de pessoas)", ylim=c(70,450))
+lines(estsinal_ma1ent, col = "blue", lty = 1, lwd = 2)
+lines(multsinal_semcorr_ent, col= "red", lty = 1, lwd = 2)
+lines(ICinf_ent, col = "black", lty = 2)
+lines(ICsup_ent, col = "black", lty = 2)
+legend("topleft", legend = c("Desocupação", "Sinal univariado - MA(1)","Sinal multivariado - MA(1)",
+                             "IC 95% - estimativa direta"),
+       col = c("black","blue","red","black"),lty = c(1,1,1,2),lwd = c(2,2,2,1),bty = "n", cex=0.8)
+
+plot((cv_ent*100), type = "l", col = "black", lwd = 2,
+     xlab = "Ano", ylab = "CV (%)",  ylim=c(2,13))
+lines(estcv_ma1ent, col = "blue",lwd=2, lty = 1)
+lines(cv_multsinal_semcorr_ent, col = "red",lwd=2,lty = 1)
+legend("topleft", legend = c("CV desocupados","CV sinal - univariado", "CV sinal - multivariado"),
+       col = c("black","blue", "red"),lty = c(1,1,1),lwd = c(2,2,2),bty = "n", cex=0.8)
+mtext("02 - Entorno metropolitano de Belo Horizonte", side = 3, outer = TRUE, line = 0.5, font = 2, cex = 1.2)
+
+
+
+### 03 - SUL DE MINAS ##########################################################
+rm(list = ls())
+
+env1<-new.env()
+env2<-new.env()
+
+load("D:/FJP2425/Programacao/data/Rdatas/6_estruturaldesocup_8reg/03_mod_sul.Rdata", envir = env1)
+load("D:/FJP2425/Programacao/data/Rdatas/11_multivariado_semcorr - desoc_8reg/02_mod_semcorr_SULARMA11.Rdata", envir = env2)
+
+baseestr8reg <- readRDS("D:/FJP2425/Programacao/data/baseestr8reg.RDS")
+sul<-baseestr8reg$`03-Sul de Minas`
+baseal8reg<- readRDS("D:/FJP2425/Programacao/data/basealinhada_8reg.RDS")
+dtsul<-baseal8reg$`03-Sul de Minas` ## Arquivo "cru", saída direta da rotina da base por rotação
+dbsul<-readRDS("D:/FJP2425/Programacao/data/pseudoerros_8reg/03_params_sul.RDS") ## Arquivo retirado da rotina de elaboração dos pseudo erros
+
+desoc_sul <- sul$Total.de.desocupados/1000
+se_db <- sul$sd_d/1000
+cv_sul <- se_db/desoc_sul
+ICinf_sul<-desoc_sul-1.96*se_db
+ICsup_sul<-desoc_sul+1.96*se_db
+
+desoc_sul <- window(ts.union(ts(desoc_sul, start = 2012, frequency = 4)), start = c(2013,4))
+ICinf_sul <- window(ts.union(ts(ICinf_sul, start = 2012, frequency = 4)), start = c(2013,4))
+ICsup_sul <- window(ts.union(ts(ICsup_sul, start = 2012, frequency = 4)), start = c(2013,4))
+cv_sul <- window(ts.union(ts(cv_sul, start = 2012, frequency = 4)), start = c(2013,4))
+
+estsinal_arma11sul <- env1$arma11_sul$ts.signal
+estsinal_arma11sul <- window(ts.union(ts(estsinal_arma11sul, start = 2012, frequency = 4)), start = c(2013,4))
+estcv_arma11sul <- env1$arma11_sul$cv.signal
+estcv_arma11sul <- window(ts.union(ts(estcv_arma11sul, start = 2012, frequency = 4)), start = c(2013,4))
+
+multsinal_semcorr_sul <- env2$modelo_mult_sem_corr$ts.signal_3
+multsinal_semcorr_sul <- window(ts.union(ts(multsinal_semcorr_sul, start = 2012, frequency = 4)), start = c(2013,4))
+cv_multsinal_semcorr_sul <- env2$modelo_mult_sem_corr$cv.signal_3
+cv_multsinal_semcorr_sul <- window(ts.union(ts(cv_multsinal_semcorr_sul, start = 2012, frequency = 4)), start = c(2013,4))
+
+par(mfrow=c(1,2), mar=c(5,5,1,1), oma=c(0,0,2,0), cex=0.8)
+plot(desoc_sul, type = "l", col = "black", lwd = 2,
+     xlab = "Ano", ylab = "Total de desocupados (milhares de pessoas)", ylim=c(15,230))
+lines(estsinal_arma11sul, col = "blue", lty = 1, lwd = 2)
+lines(multsinal_semcorr_sul, col= "red", lty = 1, lwd = 2)
+lines(ICinf_sul, col = "black", lty = 2)
+lines(ICsup_sul, col = "black", lty = 2)
+legend("topleft", legend = c("Desocupação", "Sinal univariado - ARMA(1,1)","Sinal multivariado - ARMA(1,1)",
+                             "IC 95% - estimativa direta"),
+       col = c("black","blue","red","black"),lty = c(1,1,1,2),lwd = c(2,2,2,1),bty = "n", cex=0.8)
+
+plot((cv_sul*100), type = "l", col = "black", lwd = 2,
+     xlab = "Ano", ylab = "CV (%)",  ylim=c(5,20))
+lines(estcv_arma11sul, col = "blue",lwd=2, lty = 1)
+lines(cv_multsinal_semcorr_sul, col = "red",lwd=2,lty = 1)
+legend("topleft", legend = c("CV desocupados","CV sinal - univariado", "CV sinal - multivariado"),
+       col = c("black","blue", "red"),lty = c(1,1,1),lwd = c(2,2,2),bty = "n", cex=0.8)
+mtext("03 - Sul de Minas", side = 3, outer = TRUE, line = 0.5, font = 2, cex = 1.2)
+
+
+### 04 - TRIÂNGULO MINEIRO #####################################################
+rm(list = ls())
+
+env1<-new.env()
+env2<-new.env()
+
+load("D:/FJP2425/Programacao/data/Rdatas/6_estruturaldesocup_8reg/04_mod_trg.Rdata", envir = env1)
+load("D:/FJP2425/Programacao/data/Rdatas/11_multivariado_semcorr - desoc_8reg/02_mod_semcorr_SULARMA11.Rdata", envir = env2)
+
+baseestr8reg <- readRDS("D:/FJP2425/Programacao/data/baseestr8reg.RDS")
+trg<-baseestr8reg$`04-Triângulo Mineiro`
+baseal8reg<- readRDS("D:/FJP2425/Programacao/data/basealinhada_8reg.RDS")
+dttrg<-baseal8reg$`04-Triângulo Mineiro` 
+dbtrg<-readRDS("D:/FJP2425/Programacao/data/pseudoerros_8reg/04_params_trg.RDS") 
+
+desoc_trg <- trg$Total.de.desocupados/1000
+se_db <- trg$sd_d/1000
+cv_trg <- se_db/desoc_trg
+ICinf_trg<-desoc_trg-1.96*se_db
+ICsup_trg<-desoc_trg+1.96*se_db
+
+desoc_trg <- window(ts.union(ts(desoc_trg, start = 2012, frequency = 4)), start = c(2013,4))
+ICinf_trg <- window(ts.union(ts(ICinf_trg, start = 2012, frequency = 4)), start = c(2013,4))
+ICsup_trg <- window(ts.union(ts(ICsup_trg, start = 2012, frequency = 4)), start = c(2013,4))
+cv_trg <- window(ts.union(ts(cv_trg, start = 2012, frequency = 4)), start = c(2013,4))
+
+estsinal_ma1trg <- env1$ma1_trg$ts.signal
+estsinal_ma1trg <- window(ts.union(ts(estsinal_ma1trg, start = 2012, frequency = 4)), start = c(2013,4))
+estcv_ma1trg <- env1$ma1_trg$cv.signal
+estcv_ma1trg <- window(ts.union(ts(estcv_ma1trg, start = 2012, frequency = 4)), start = c(2013,4))
+
+multsinal_semcorr_trg <- env2$modelo_mult_sem_corr$ts.signal_4
+multsinal_semcorr_trg <- window(ts.union(ts(multsinal_semcorr_trg, start = 2012, frequency = 4)), start = c(2013,4))
+cv_multsinal_semcorr_trg <- env2$modelo_mult_sem_corr$cv.signal_4
+cv_multsinal_semcorr_trg <- window(ts.union(ts(cv_multsinal_semcorr_trg, start = 2012, frequency = 4)), start = c(2013,4))
+
+par(mfrow=c(1,2), mar=c(5,5,1,1), oma=c(0,0,2,0), cex=0.8)
+plot(desoc_trg, type = "l", col = "black", lwd = 2,
+     xlab = "Ano", ylab = "Total de desocupados (milhares de pessoas)", ylim=c(30,215))
+lines(estsinal_ma1trg, col = "blue", lty = 1, lwd = 2)
+lines(multsinal_semcorr_trg, col= "red", lty = 1, lwd = 2)
+lines(ICinf_trg, col = "black", lty = 2)
+lines(ICsup_trg, col = "black", lty = 2)
+legend("topleft", legend = c("Desocupação", "Sinal univariado - MA(1)","Sinal multivariado - MA(1)",
+                             "IC 95% - estimativa direta"),
+       col = c("black","blue","red","black"),lty = c(1,1,1,2),lwd = c(2,2,2,1),bty = "n", cex=0.8)
+
+plot((cv_trg*100), type = "l", col = "black", lwd = 2,
+     xlab = "Ano", ylab = "CV (%)",  ylim=c(4,18))
+lines(estcv_ma1trg, col = "blue",lwd=2, lty = 1)
+lines(cv_multsinal_semcorr_trg, col = "red",lwd=2,lty = 1)
+legend("topleft", legend = c("CV desocupados","CV sinal - univariado", "CV sinal - multivariado"),
+       col = c("black","blue", "red"),lty = c(1,1,1),lwd = c(2,2,2),bty = "n", cex=0.8)
+mtext("04 - Triângulo mineiro", side = 3, outer = TRUE, line = 0.5, font = 2, cex = 1.2)
+
+### 05 - ZONA DA MATA ##########################################################
+rm(list = ls())
+
+env1<-new.env()
+env2<-new.env()
+
+load("D:/FJP2425/Programacao/data/Rdatas/6_estruturaldesocup_8reg/05_mod_mat.Rdata", envir = env1)
+load("D:/FJP2425/Programacao/data/Rdatas/11_multivariado_semcorr - desoc_8reg/02_mod_semcorr_SULARMA11.Rdata", envir = env2)
+
+baseestr8reg <- readRDS("D:/FJP2425/Programacao/data/baseestr8reg.RDS")
+mat<-baseestr8reg$`05-Mata de Minas Gerais`
+baseal8reg<- readRDS("D:/FJP2425/Programacao/data/basealinhada_8reg.RDS")
+dtmat<-baseal8reg$`05-Mata de Minas Gerais`
+dbmat<-readRDS("D:/FJP2425/Programacao/data/pseudoerros_8reg/05_params_mat.RDS")
+
+desoc_mat<- mat$Total.de.desocupados/1000
+se_db <- mat$sd_d/1000
+cv_mat <- se_db/desoc_mat
+ICinf_mat<-desoc_mat-1.96*se_db
+ICsup_mat<-desoc_mat+1.96*se_db
+
+desoc_mat <- window(ts.union(ts(desoc_mat, start = 2012, frequency = 4)), start = c(2013,4))
+ICinf_mat <- window(ts.union(ts(ICinf_mat, start = 2012, frequency = 4)), start = c(2013,4))
+ICsup_mat <- window(ts.union(ts(ICsup_mat, start = 2012, frequency = 4)), start = c(2013,4))
+cv_mat <- window(ts.union(ts(cv_mat, start = 2012, frequency = 4)), start = c(2013,4))
+
+estsinal_ma1mat <- env1$ma1_mat$ts.signal
+estsinal_ma1mat <- window(ts.union(ts(estsinal_ma1mat, start = 2012, frequency = 4)), start = c(2013,4))
+estcv_ma1mat <- env1$ma1_mat$cv.signal
+estcv_ma1mat <- window(ts.union(ts(estcv_ma1mat, start = 2012, frequency = 4)), start = c(2013,4))
+
+multsinal_semcorr_mat <- env2$modelo_mult_sem_corr$ts.signal_5
+multsinal_semcorr_mat <- window(ts.union(ts(multsinal_semcorr_mat, start = 2012, frequency = 4)), start = c(2013,4))
+cv_multsinal_semcorr_mat <- env2$modelo_mult_sem_corr$cv.signal_5
+cv_multsinal_semcorr_mat <- window(ts.union(ts(cv_multsinal_semcorr_mat, start = 2012, frequency = 4)), start = c(2013,4))
+
+par(mfrow=c(1,2), mar=c(5,5,1,1), oma=c(0,0,2,0), cex=0.8)
+plot(desoc_mat, type = "l", col = "black", lwd = 2,
+     xlab = "Ano", ylab = "Total de desocupados (milhares de pessoas)", ylim=c(25,210))
+lines(estsinal_ma1mat, col = "blue", lty = 1, lwd = 2)
+lines(multsinal_semcorr_mat, col= "red", lty = 1, lwd = 2)
+lines(ICinf_mat, col = "black", lty = 2)
+lines(ICsup_mat, col = "black", lty = 2)
+legend("topleft", legend = c("Desocupação", "Sinal univariado - MA(1)","Sinal multivariado - MA(1)",
+                             "IC 95% - estimativa direta"),
+       col = c("black","blue","red","black"),lty = c(1,1,1,2),lwd = c(2,2,2,1),bty = "n", cex=0.8)
+
+plot((cv_mat*100), type = "l", col = "black", lwd = 2,
+     xlab = "Ano", ylab = "CV (%)",  ylim=c(3,18))
+lines(estcv_ma1mat, col = "blue",lwd=2, lty = 1)
+lines(cv_multsinal_semcorr_mat, col = "red",lwd=2,lty = 1)
+legend("topleft", legend = c("CV desocupados","CV sinal - univariado", "CV sinal - multivariado"),
+       col = c("black","blue", "red"),lty = c(1,1,1),lwd = c(2,2,2),bty = "n", cex=0.8)
+mtext("05 - Zona da Mata", side = 3, outer = TRUE, line = 0.5, font = 2, cex = 1.2)
+
+
+### 06 - NORTE DE MINAS ########################################################
+rm(list = ls())
+
+env1<-new.env()
+env2<-new.env()
+
+load("D:/FJP2425/Programacao/data/Rdatas/6_estruturaldesocup_8reg/06_mod_nrt.Rdata", envir = env1)
+load("D:/FJP2425/Programacao/data/Rdatas/11_multivariado_semcorr - desoc_8reg/02_mod_semcorr_SULARMA11.Rdata", envir = env2)
+
+baseestr8reg <- readRDS("D:/FJP2425/Programacao/data/baseestr8reg.RDS")
+nrt<-baseestr8reg$`06-Norte de Minas`
+baseal8reg<- readRDS("D:/FJP2425/Programacao/data/basealinhada_8reg.RDS")
+dtnrt<-baseal8reg$`06-Norte de Minas`
+dbnrt<-readRDS("D:/FJP2425/Programacao/data/pseudoerros_8reg/06_params_nrt.RDS") 
+
+desoc_nrt <- nrt$Total.de.desocupados/1000
+se_db <- nrt$sd_d/1000
+cv_nrt <- se_db/desoc_nrt
+ICinf_nrt<-desoc_nrt-1.96*se_db
+ICsup_nrt<-desoc_nrt+1.96*se_db
+
+desoc_nrt <- window(ts.union(ts(desoc_nrt, start = 2012, frequency = 4)), start = c(2013,4))
+ICinf_nrt <- window(ts.union(ts(ICinf_nrt, start = 2012, frequency = 4)), start = c(2013,4))
+ICsup_nrt <- window(ts.union(ts(ICsup_nrt, start = 2012, frequency = 4)), start = c(2013,4))
+cv_nrt <- window(ts.union(ts(cv_nrt, start = 2012, frequency = 4)), start = c(2013,4))
+
+estsinal_ma1nrt <- env1$ma1_nrt$ts.signal
+estsinal_ma1nrt <- window(ts.union(ts(estsinal_ma1nrt, start = 2012, frequency = 4)), start = c(2013,4))
+estcv_ma1nrt <- env1$ma1_nrt$cv.signal
+estcv_ma1nrt <- window(ts.union(ts(estcv_ma1nrt, start = 2012, frequency = 4)), start = c(2013,4))
+
+multsinal_semcorr_nrt <- env2$modelo_mult_sem_corr$ts.signal_6
+multsinal_semcorr_nrt <- window(ts.union(ts(multsinal_semcorr_nrt, start = 2012, frequency = 4)), start = c(2013,4))
+cv_multsinal_semcorr_nrt <- env2$modelo_mult_sem_corr$cv.signal_6
+cv_multsinal_semcorr_nrt <- window(ts.union(ts(cv_multsinal_semcorr_nrt, start = 2012, frequency = 4)), start = c(2013,4))
+
+par(mfrow=c(1,2), mar=c(5,5,1,1), oma=c(0,0,2,0), cex=0.8)
+plot(desoc_nrt, type = "l", col = "black", lwd = 2,
+     xlab = "Ano", ylab = "Total de desocupados (milhares de pessoas)", ylim=c(40,280))
+lines(estsinal_ma1nrt, col = "blue", lty = 1, lwd = 2)
+lines(multsinal_semcorr_nrt, col= "red", lty = 1, lwd = 2)
+lines(ICinf_nrt, col = "black", lty = 2)
+lines(ICsup_nrt, col = "black", lty = 2)
+legend("topleft", legend = c("Desocupação", "Sinal univariado - MA(1)","Sinal multivariado - MA(1)",
+                             "IC 95% - estimativa direta"),
+       col = c("black","blue","red","black"),lty = c(1,1,1,2),lwd = c(2,2,2,1),bty = "n", cex=0.8)
+
+plot((cv_nrt*100), type = "l", col = "black", lwd = 2,
+     xlab = "Ano", ylab = "CV (%)",  ylim=c(2.5,22))
+lines(estcv_ma1nrt, col = "blue",lwd=2, lty = 1)
+lines(cv_multsinal_semcorr_nrt, col = "red",lwd=2,lty = 1)
+legend("topleft", legend = c("CV desocupados","CV sinal - univariado", "CV sinal - multivariado"),
+       col = c("black","blue", "red"),lty = c(1,1,1),lwd = c(2,2,2),bty = "n", cex=0.8)
+mtext("06 - Norte de Minas", side = 3, outer = TRUE, line = 0.5, font = 2, cex = 1.2)
+
+
+
+
+
+
+
+
+
+
